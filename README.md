210a2c8506a95efed2b6243d4c46766427d5f92e
DANIELLE ALBA <danib.sgh@gmail.com>
2019-12-01 19:52:56 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Creating and Inserting DOM Nodes
+\# Creating and Inserting DOM Nodes
 
 ## Learning Goals
 
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+var element = document.createElement('div');
+document.body.appendChild(element);
+var ul = document.createElement('ul');
+
+for( let i = 0; i < 3; i++){
+  let li = document.createElement('li')
+  li.innerHTML = ( i + 1).toString()
+  ul.appendChild(li)
+}
+
+element.appendChild(ul)
